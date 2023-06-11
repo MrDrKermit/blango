41a3dd9d95133fbea17f67e4fb5e2eda1f3dd6d0
Alexander Bernath <alexander.bernath@simutence.de>
2023-06-11 11:16:42 +0000

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -62,6 +62,8 @@ class Dev(Configuration):
         'allauth.account',
         'allauth.socialaccount',
         'allauth.socialaccount.providers.google',
+
+        'rest_framework',
     ]
 
     # allauth settings
